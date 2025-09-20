8d455cf04e74307384cd298e1b3b6ba25a104cf0
cvicens <carlos.vicens.alonso@gmail.com>
2025-09-20 11:03:42 +0200

    close

--- helm/run.sh
+++ helm/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 APP_NAME=eligibility
 
-helm template . --name-template ${APP_NAME} \
-  --include-crds
+helm template . --values intel.yaml --version 0.1.0 --namespace ${APP_NAME} --name-template ${APP_NAME} \
+  --include-crds --debug
