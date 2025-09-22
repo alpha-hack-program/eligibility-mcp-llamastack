f8b25295136caca120868b96781e8382861c6883
cvicens <carlos.vicens.alonso@gmail.com>
2025-09-22 15:17:03 +0200

    tested on rhoai 2.23 on AWS

--- helm/run.sh
+++ helm/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 APP_NAME=eligibility
+# VALUES="--values intel.yaml"
+# VALUES="--values nvidia.yaml"
+VALUES=""
 
 helm template . --name-template ${APP_NAME} \
-  --include-crds
+  --include-crds ${VALUES} 
+  
