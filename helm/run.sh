2a950d907f804b35f8c7768db610a8bf9291b6af
cvicens <carlos.vicens.alonso@gmail.com>
2025-10-01 15:18:39 +0200

    New MCP server versions

--- helm/run.sh
+++ helm/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 APP_NAME=eligibility
 # VALUES="--values intel.yaml"
-# VALUES="--values nvidia.yaml"
-VALUES=""
+VALUES="--values nvidia.yaml"
+# VALUES=""
 
 helm template . --name-template ${APP_NAME} \
   --include-crds ${VALUES} 
